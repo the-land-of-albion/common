054053fc39b1767ecf71fa440f952ca9fdfc00dd
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2021-02-26 11:50:52 +0100

    changed typing on fetch

--- lib/OptionBuilder.ts
+++ lib/OptionBuilder.ts
@@ -1,7 +1,6 @@
 import { Options, RequestKeys } from "./Options";
 import fetch from "node-fetch";
 
-
 export class OptionsBuilder{
     private headers?: Record<string,any>
     constructor(headers?){
@@ -17,11 +16,11 @@ export class OptionsBuilder{
         this.headers = {...this.headers, ...headers};
         return this;
     }
-    fetch(url, req: RequestKeys, optionals?: {body?: any, headers?: Record<string, any>}){
+    fetch(url, req: RequestKeys, optionals?: {body?: any, headers?: Record<string, any>}): Promise<any>{
         const usedHeaders = {...this.headers,...optionals?.headers }; 
         const builderOptionals = { body: optionals?.body, headers: usedHeaders}
         const options = new Options(req, builderOptionals).transform();
-        return fetch(url, options);
+        return fetch(url, options) as Promise<any>;
     }
 }
 
